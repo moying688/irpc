42299caf44a9fa3e9d581e00baa82d6fd6449f5b
moying <2869289913@qq.com>
2024-12-07 14:35:16 +0800

    first commit

---
+++ irpc-core/src/main/java/org/moying/common/cache/CommonServerCache.java
@@ -0,0 +1,8 @@
+package org.moying.common.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonServerCache {
+    public static Map<String, Object> PROVIDER_CLASS_MAP = new HashMap<>();
+}
---
+++ irpc-core/src/main/java/org/moying/common/constants/RpcConstants.java
@@ -0,0 +1,6 @@
+package org.moying.common.constants;
+
+public interface RpcConstants {
+
+   short MAGIC_NUMBER  = 1 ;
+}
